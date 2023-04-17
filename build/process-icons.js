e1d9328706e3075d081677a0fe7e7247a5d16038
Kloen <kloen@kloenlansfiel.com>
2023-04-17 15:24:23 +0200

    style: Run prettier over the build folder

--- build/process-icons.js
+++ build/process-icons.js
@@ -1,22 +1,22 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs"
+import path from "path"
 import svgo from "svgo"
 import prettier from "prettier"
-import { minify } from 'html-minifier';
+import { minify } from "html-minifier"
 
 const svgoConfig = {
     plugins: [
         {
-            name: 'preset-default',
+            name: "preset-default",
             params: {
                 overrides: {
                     convertShapeToPath: false,
-                    mergePaths: false,
-                },
-            },
-        },
-    ],
-};
+                    mergePaths: false
+                }
+            }
+        }
+    ]
+}
 
 const prettierConfig = {
     tabWidth: 2,
@@ -26,35 +26,35 @@ const prettierConfig = {
 
 const processIcons = (dir) =>
     fs.readdirSync(dir).flatMap((subdir) => {
-        const subdirPath = path.join(dir, subdir);
+        const subdirPath = path.join(dir, subdir)
         if (fs.statSync(subdirPath).isDirectory()) {
             return fs
                 .readdirSync(subdirPath)
                 .filter((file) => path.extname(file) === ".svg")
                 .map((file) => {
-                    const filePath = path.join(subdirPath, file);
-                    const content = fs.readFileSync(filePath, "utf8");
+                    const filePath = path.join(subdirPath, file)
+                    const content = fs.readFileSync(filePath, "utf8")
 
                     const optimizedContent = svgo.optimize(content, svgoConfig).data
                     const formattedContent = prettier.format(optimizedContent, prettierConfig)
                     fs.writeFileSync(filePath, formattedContent)
 
-                    const svgContent = optimizedContent.match(/<svg[^>]*>([\s\S]*?)<\/svg>/i);
-                    const value = svgContent ? svgContent[1] : "";
+                    const svgContent = optimizedContent.match(/<svg[^>]*>([\s\S]*?)<\/svg>/i)
+                    const value = svgContent ? svgContent[1] : ""
                     const minifiedValue = minify(value, {
                         collapseWhitespace: true,
-                        removeComments: true,
-                    });
+                        removeComments: true
+                    })
                     return {
                         name: path.basename(file, ".svg"),
                         type: subdir,
-                        value: minifiedValue,
-                    };
-                });
+                        value: minifiedValue
+                    }
+                })
         }
-        return [];
-    });
+        return []
+    })
 
-const icons = processIcons("icons");
-const json = JSON.stringify(icons, null, 2);
-fs.writeFileSync("src/generated/icons.json", json);
+const icons = processIcons("icons")
+const json = JSON.stringify(icons, null, 2)
+fs.writeFileSync("src/generated/icons.json", json)
